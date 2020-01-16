17f1805695ea9f6cca3c731379b2b54026d722b9
navib <ivan.baena.dev@gmail.com>
2020-01-15 22:52:16 -0500

    redux-thunk added

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-import FormContainer from "./js/components/container/FormContainer.jsx";
+import React from "react";
+import { render } from "react-dom";
+import { Provider } from "react-redux";
+import store from "./js/store/index";
+import App from "./js/components/presentational/App";
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
---
+++ src/js/components/presentational/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import List from "../containers/List";
+import Form from "./Form";
+import Post from "../containers/Posts";
+
+const App = () => (
+  <>
+    <div>
+      <h2>Articles</h2>
+      <List />
+    </div>
+    <div>
+      <h2>Add a new article</h2>
+      <Form />
+    </div>
+    <div>
+      <h2>API posts</h2>
+      <Post />
+    </div>
+  </>
+);
+
+export default App;
